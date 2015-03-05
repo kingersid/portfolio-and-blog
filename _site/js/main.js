aa3769878432c9bcac6bcf4bd0c767e31ecfd943
kingersid <kinger.siddharth@gmail.com>
2015-03-05 21:55:47 +0530

    adding pages second time

--- _site/js/main.js
+++
@@ -1,9 +0,0 @@
-var $container = $('.portfolio-items').isotope('layout');
-
-$('.portfolio-filter').on( 'click', 'a', function(e) {
-  e.preventDefault();
-  $('.portfolio-filter li').removeClass('active');
-  $(this).closest('li').addClass('active');
-  var filterValue = $(this).attr('data-filter');
-  $container.isotope({ filter: filterValue });
-});
